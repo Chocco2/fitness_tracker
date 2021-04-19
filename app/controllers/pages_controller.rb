600775a8881ee614df9211b3474773e2ac3b2a9f
Chocco2 <qcioccolini@gmail.com>
2021-04-19 18:55:44 +0200

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def home #Tells rails that the home action called from routes.rb has to render home.html.erb from views/pages
+  end
+
+  def about
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#home'
+  # get 'about', to: 'pages#about' --> Tell rails that localhost:3000/about has to call the about action from pages_controller
+end
